1f1cac78f8b3c113e44da89b6f05b8aed6bee914
Polume <alex.olivier.2502@gmail.com>
2023-03-08 18:34:43 +0100

    Revert "je git en premier cheh"
    This reverts commit 9d843a0e087675269dd492f86b243516d02535c7.

--- GUI.h
+++ GUI.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-
 void run();
 
 #endif
--- GUI_init.h
+++ GUI_init.h
@@ -5,14 +5,9 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-
-#define NOIR {255,255,255}
-#define BLANC {0,0,0}
-#define VERT {17,40,32}
-
 SDL_Window *createWindow(int width, int height);
 SDL_Rect newRect(int x, int y, int width, int height);
-int Place_BG(SDL_Window * window, SDL_Renderer * renderer, int width, int height, int * BG_color);
+int Place_Rect(SDL_Window * window, SDL_Renderer * renderer, int width, int height, int * BG_color);
 void Quit_GUI(SDL_Window * window, SDL_Renderer * renderer);
 int init_GUI(SDL_Window * window,SDL_Renderer * renderer, int * BG_color); //On devra placer le tableau de matrice representant les cases de l'othello.
 
